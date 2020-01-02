19725ba979a58b0f71d4104e9b85f0acf05407af
511377909@qq.com <you@example.com>
2020-01-02 17:05:41 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="wrapper">
+   <swiper :options="swiperOption">
+    <swiper-slide  v-for="item of swiperList" :key="item.id">
+        <img  class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HomeSwiper',
+  data () {
+    return {
+        swiperOption:{
+            pagination:'.swiper-pagination',
+            loop:true
+      },
+      swiperList: [{
+          id:'0001',
+          imgUrl:'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/0e1edf7862893c52952810529c69b389.jpg_890x330_c9158c29.jpg'
+      },{
+          id:'0002',
+          imgUrl:'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/9aaf9b0616480131ee3997051824cc33.jpg_890x330_4ecb4794.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+ .wrapper >>> .swiper-pagination-bullet-active
+      background:#fff
+     display :attribute
+ .wrapper
+  overflow :hidden
+  width:100% 
+  height:0
+  padding-bottom:31.25%
+  background:#eee
+ .swiper-img
+     width:100%
+</style>
